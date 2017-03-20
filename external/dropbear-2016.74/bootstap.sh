41a0f5cd7bf07c462b924250c44cf981b5b71559
Jackson Chen <jchen@sayhello.com>
2017-03-20 14:47:18 -0700

    delete nodes

--- external/dropbear-2016.74/bootstap.sh
+++ external/dropbear-2016.74/bootstap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PATH=$PATH:/usr/local/linaro-armv7ahf-2015.11-gcc5.2/bin
-./configure --prefix=$($PWD) CC=arm-linux-gnueabihf-gcc --host=arm --disable-zlib
+./configure --prefix=$($PWD)/out CC=arm-linux-gnueabihf-gcc --host=arm --disable-zlib
 make
