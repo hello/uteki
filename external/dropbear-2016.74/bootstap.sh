257319167d055dfbd73e4dedc5620e3a11730eaa
Jackson Chen <jchen@sayhello.com>
2017-03-20 15:09:46 -0700

    update

--- external/dropbear-2016.74/bootstap.sh
+++ external/dropbear-2016.74/bootstap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./configure --prefix=$PWD/out CC=arm-linux-gnueabihf-gcc --host=arm --disable-zlib
-make
+./configure --prefix="$PWD/out" CC=arm-linux-gnueabihf-gcc --host=arm --disable-zlib
+make && make install
