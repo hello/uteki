698ab405bb1c908ae5eaa28a43f81f28b51e714b
plasticchris <plasticchris@gmail.com>
2017-03-21 22:30:13 -0700

    add compiler path to bootstrap so it doesn't fall back to native

--- external/zeromq-4.2.1/bootstrap.sh
+++ external/zeromq-4.2.1/bootstrap.sh
@@ -1,4 +1,5 @@
-./configure --host=arm-linux-gnueabihf --prefix=$PWD/out
+PATH=$PATH:/usr/local/linaro-armv7ahf-2015.11-gcc5.2/bin
+./configure  --host=arm-linux-gnueabihf --prefix=$PWD/out
 make -j $(nproc) && make install
 mv $PWD/out/* $PWD/../../prebuild/libzmq/usr/
 
