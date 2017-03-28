0c62b0a88fbbf217c134ab3590d8434c8f3c4ee5
plasticchris <plasticchris@gmail.com>
2017-03-28 10:50:47 -0700

    install libzmq to the prebuild directory
    to build against it be like:
    
    /usr/local/linaro-armv7ahf-2015.11-gcc5.2/bin/arm-linux-gnueabihf-gcc ` pkg-config --libs --cflags libzmq.pc ` hw.cpp -o hwcpp

--- external/zeromq-4.2.1/bootstrap.sh
+++ external/zeromq-4.2.1/bootstrap.sh
@@ -1,5 +1,4 @@
 PATH=$PATH:/usr/local/linaro-armv7ahf-2015.11-gcc5.2/bin
-./configure  --host=arm-linux-gnueabihf --prefix=$PWD/out
+./configure  --host=arm-linux-gnueabihf --prefix=$PWD/../../prebuild/libzmq/usr/
 make -j $(nproc) && make install
-mv $PWD/out/* $PWD/../../prebuild/libzmq/usr/
 
